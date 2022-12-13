8bdff104f63f9fb51580b13898e9269b8c59bf20
Stuart <47339755+BearintheWillows@users.noreply.github.com>
2022-12-13 13:30:36 +0000

    Changed Namespace

--- Todo.WillowBear/Todo.WillowBear/Data/TodoDbContext.cs
+++ Todo.WillowBear/Todo.WillowBear/Data/TodoDbContext.cs
@@ -1,19 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.WillowBear.Models;
 
-namespace Todo.WillowBear.Data
+namespace Todo.WillowBear.Data;
+public class TodoDbContext : DbContext
 {
-    public class TodoDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=TodoList.db");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new TodoItemConfiguration());
-        }
-        public DbSet<TodoItem> TodoItems { get; set; }
+        optionsBuilder.UseSqlite("Data Source=TodoList.db");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new TodoItemConfiguration());
+    }
+    public DbSet<TodoItem> TodoItems { get; set; }
 }
