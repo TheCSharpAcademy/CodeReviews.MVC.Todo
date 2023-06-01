050eec8e468c6c3ba405ac2dcefa4abaa4225aa8
heartingNinja <neilfosteronly@gmail.com>
2023-06-01 18:12:07 +0700

    First Attempt ToDo

--- Todo.heartingNinja/TodoApi/TodoApi/Models/TodoContext.cs
+++ Todo.heartingNinja/TodoApi/TodoApi/Models/TodoContext.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.CodeAnalysis;
 
-namespace TodoApi.Models
+namespace TodoApi.Models;
+
+public class TodoContext : DbContext
 {
-    public class TodoContext : DbContext
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
     {
-        public TodoContext(DbContextOptions<TodoContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<TodoItem> TodoItems { get; set; } = null!;
     }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
 }
--- Todo.heartingNinja/TodoApi/TodoApi/Program.cs
+++ Todo.heartingNinja/TodoApi/TodoApi/Program.cs
@@ -3,25 +3,17 @@ using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
-//});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseDefaultFiles();
