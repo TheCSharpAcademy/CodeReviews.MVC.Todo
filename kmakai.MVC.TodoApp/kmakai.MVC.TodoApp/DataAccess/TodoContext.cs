6ed48680f0eb37c668349d0d565108fd11d0fbf4
kmakai <makaikwag@gmail.com>
2023-08-17 14:14:46 -0400

    review submission

---
+++ kmakai.MVC.TodoApp/kmakai.MVC.TodoApp/DataAccess/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using kmakai.MVC.TodoApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace kmakai.MVC.TodoApp.DataAccess;
+
+public class TodoContext: DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = default!;
+}
+
