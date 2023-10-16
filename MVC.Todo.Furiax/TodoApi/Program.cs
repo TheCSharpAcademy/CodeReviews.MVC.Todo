8fd6794da4305f97c228de2a96553255b38e6ef9
furiax <carlmalfliet@proximus.be>
2023-10-16 10:13:03 +0200

    added context and deleted weatherforecast

--- MVC.Todo.Furiax/TodoApi/Program.cs
+++ MVC.Todo.Furiax/TodoApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.DataAccess;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString") 
+    ?? throw new Exception("Connectionstring not found.")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
