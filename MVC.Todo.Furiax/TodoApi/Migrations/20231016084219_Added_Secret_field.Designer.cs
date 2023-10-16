dfa9c46da02814a4a1d26d67657f73fc91f79a22
furiax <carlmalfliet@proximus.be>
2023-10-16 10:51:23 +0200

    removed use swagger from program.cs and replaced it with use default/static files

--- MVC.Todo.Furiax/TodoApi/Migrations/20231016084219_Added_Secret_field.Designer.cs
+++ MVC.Todo.Furiax/TodoApi/Migrations/20231016084219_Added_Secret_field.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoApi.DataAccess;
 
@@ -10,9 +11,11 @@ using TodoApi.DataAccess;
 namespace TodoApi.Migrations
 {
     [DbContext(typeof(TodoContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20231016084219_Added_Secret_field")]
+    partial class Added_Secret_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MVC.Todo.Furiax/TodoApi/Program.cs
+++ MVC.Todo.Furiax/TodoApi/Program.cs
@@ -7,20 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")
-    ?? throw new Exception("Connectionstring not found.")));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
