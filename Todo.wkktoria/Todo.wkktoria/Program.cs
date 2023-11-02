0474536f6960e0e41befd91ee9a716b952441505
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-11-02 20:52:56 +0100

    Create API

--- Todo.wkktoria/Todo.wkktoria/Program.cs
+++ Todo.wkktoria/Todo.wkktoria/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.wkktoria.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
