f89b72cae04071b5d88ad1923e1ff365c5ea8c90
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-01-22 16:40:06 +0100

    Added css styling, images and part of the fetch api implementation

--- MVC.Todo.K-MYR/MVC.Todo.K-MYR/Data/SeedData.cs
+++ MVC.Todo.K-MYR/MVC.Todo.K-MYR/Data/SeedData.cs
@@ -5,7 +5,7 @@ namespace MVC.Todo.K_MYR.Data;
 public static class SeedData
 {
     public static void Initialize(DatabaseContext db)
-    {
+    {        
         if(!db.Todos.Any())
         {
             db.Todos.AddRange(
@@ -13,13 +13,36 @@ public static class SeedData
                 {
                     Name = "Laundry",
                     IsCompleted = false,
-                    Group = "Household Chores"
+                    Group = "Household Chores",
+                    Description = "I need to do my dirty laundry until tomorrow"
                 },
                 new TodoTask
                 {
                     Name = "Go for a run",
                     IsCompleted = false,
-                    Group = "Exercise"
+                    Group = "Exercise",
+                    Description = "i will go for a run in the park for at least 30 mins today"
+                },
+                new TodoTask
+                {
+                    Name = "Weight Training",
+                    IsCompleted = true,
+                    Group = "Exercise",
+                    Description = "I ll go to the gym and finish a push workout"
+                },
+                new TodoTask
+                {
+                    Name = "Do homework",
+                    IsCompleted = true,
+                    Group = "School",
+                    Description = "i will do my homework so i can play games afterwards for the rest of the day"
+                },
+                new TodoTask
+                {
+                    Name = "This is a test fro really long text This is a test fro really long text This is a test fro really long text ",
+                    IsCompleted = true,
+                    Group = "School",
+                    Description = "This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text This is a test fro really long text y"
                 }
             );
         }
---
+++ MVC.Todo.K-MYR/MVC.Todo.K-MYR/wwwroot/js/index.js
@@ -0,0 +1,33 @@
+﻿const uri = "https://localhost:7055/Todos";
+const pinboard = document.getElementById("pinboard-container");
+
+document.addEventListener("DOMContentLoaded", function () {
+    getTodos();
+});
+
+function getTodos() {
+    fetch(uri)
+        .then(responste => responste.json())
+        .then(data => displayTodos(data))
+        .catch(error => console.log("Unable to fetch items", error));
+}
+
+function displayTodos(data) {    
+    data.forEach(item => {        
+        let htmlOutput = `
+                <div class="postIt-container m-3">
+                    <img class="pin" src="/img/Drawing-Pin-scaled.png" alt="Pinboard pin" />
+                    <img class="checkmark${item.isCompleted ? " show" : ""}" src="/img/checkmark.png" alt="Completed Task Check Mark" />
+                    <div class="postIt">
+                        <h4 class="text-center px-2 my-2 text-truncate">
+                            ${item.name ?? ""}
+                        </h4>
+                        <hr class="mx-2 my-3" />
+                        <div class="postIt-body mx-3 overflow-auto">
+                            ${item.description ?? ""}
+                        </div>
+                    </div>
+                </div>`;
+
+        pinboard.innerHTML += htmlOutput;
+    })}
