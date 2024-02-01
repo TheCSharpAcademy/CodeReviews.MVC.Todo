039c8d1847e1bcadaea8a3163f5b5ba65063449b
Vocal <vocalnight@protonmail.com>
2024-01-31 21:18:25 -0300

    project done

---
+++ Todo.VocalNight/TodoFetchAPI/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoFetchAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
