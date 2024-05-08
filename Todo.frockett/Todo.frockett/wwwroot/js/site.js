5058ea0bbeb90f77c5a41583631d48176da7a107
frockett <crockett.d.ford@gmail.com>
2024-05-08 16:26:04 +0800

    switched to static file setup

--- Todo.frockett/Todo.frockett/wwwroot/js/site.js
+++ Todo.frockett/Todo.frockett/wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("HEY");
+
+    fetchTodos();
+
+    document.getElementById('addTodo').addEventListener('click', function () {
+        addTodo();
+    })
+})
+
+function fetchTodos() {
+    fetch('/todoitems')
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById('todoList');
+            list.innerHTML = '';
+            data.forEach(todo => {
+                const item = document.createElement('div');
+                item.textContent = todo.name;
+                list.appendChild(item);
+            });
+        })
+        .catch(error => console.error('Unable to get items.', error));
+}
+
+
+function addTodo() {
+    const todo = { title: 'New Todo', isComplete: false }; // Simplified example
+
+    fetch('/todoitems', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+        .then(response => response.json())
+        .then(() => {
+            fetchTodos(); // Reload the list after adding
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
