664bad0316c74643a8cdd038b5ef3889e69ac29c
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-08-07 11:24:04 -0400

    added to sitejs and sitecss, added controller methods

--- Todo.kwm0304/Program.cs
+++ Todo.kwm0304/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.kwm0304.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoDb>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
