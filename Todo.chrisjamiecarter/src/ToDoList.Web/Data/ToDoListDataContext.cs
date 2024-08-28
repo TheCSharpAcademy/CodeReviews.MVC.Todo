41597b2ea8a9074903f2b37fff0389fc719dcab5
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-08-28 20:03:22 +0100

    Finished project

---
+++ Todo.chrisjamiecarter/src/ToDoList.Web/Data/ToDoListDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Web.Models;
+
+namespace ToDoList.Web.Data;
+
+/// <summary>
+/// Represents the Entity Framework database context for the ToDoList application.
+/// This context is responsible for managing the database connection and providing
+/// access to the ToDoItem entities.
+/// </summary>
+/// <remarks>
+/// This class inherits from DbContext and uses the provided DbContextOptions to configure
+/// the context. It includes a DbSet property for ToDoItem entities.
+/// </remarks>
+public class ToDoListDataContext : DbContext
+{
+    #region Constructors
+
+    public ToDoListDataContext(DbContextOptions<ToDoListDataContext> options) : base(options) { }
+
+    #endregion
+    #region Properties
+
+    public DbSet<ToDoItem> ToDoItem => Set<ToDoItem>();
+
+    #endregion
+}
---
+++ Todo.chrisjamiecarter/src/ToDoList.Web/Models/ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoList.Web.Models;
+
+/// <summary>
+/// Represents a to-do item in the ToDoList application.
+/// This class contains properties for the unique identifier, name, and completion status of the to-do item.
+/// </summary>
+/// <remarks>
+/// The Id property is a GUID that uniquely identifies each to-do item.
+/// The Name property holds the name or description of the to-do item, and it can be null.
+/// The IsComplete property indicates whether the to-do item has been completed.
+/// </remarks>
+public class ToDoItem
+{
+    #region Properties
+
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    #endregion
+}
