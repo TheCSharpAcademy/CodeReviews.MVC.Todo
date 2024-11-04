a0f56ae3dae9ed279340f14f547a574083a7e99c
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-11-04 16:12:25 -0500

    Setup SQL Server, EFCore and Todo model

---
+++ MVC.Todo.tonyissa/Todo/Data/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+namespace Todo.Data;
+
+public class TodoContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<TodoItem> todos { get; set; }
+}
--- MVC.Todo.tonyissa/Todo/Program.cs
+++ MVC.Todo.tonyissa/Todo/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+{
+    string path = builder.Configuration.GetConnectionString("TodosConnection")
+        ?? throw new Exception("InvalidConfigException: Please include a valid connection string in appsettings.json");
+    opt.UseSqlServer(path);
+});
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    context.Database.EnsureCreated();
+}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
