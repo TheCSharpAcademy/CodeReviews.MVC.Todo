37472ee7a0b8fbbfbc8d89a7bf2439915e6c6cf9
jollejonas <jollevb@hotmail.com>
2024-12-21 00:55:56 +0100

    Project finished.

---
+++ Todo.jollejonas/Todo.jollejonas/Models/TodoItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Todo.jollejonas.Models;
+
+public class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(TodoItem todoItem) =>
+    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+}
