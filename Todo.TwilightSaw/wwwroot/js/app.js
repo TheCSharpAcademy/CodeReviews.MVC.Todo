1dd5b0ad1eda11a511adb580d9ccabefbc091b24
TwilightSaw <56154417+TwilightSaw@users.noreply.github.com>
2025-01-16 21:17:28 +0100

    Todo 0.037
    Added new delete function, refreshed page style

--- Todo.TwilightSaw/wwwroot/js/app.js
+++ Todo.TwilightSaw/wwwroot/js/app.js
@@ -10,44 +10,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var _a;
 function fetchTodos() {
     return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch('/todoLists');
+        const response = yield fetch("/todoLists");
         const todo = yield response.json();
-        const list = document.getElementById('todoList');
+        const list = document.getElementById("todoList");
         if (list) {
-            list.innerHTML = '';
+            list.innerHTML = "";
             todo.forEach((todo) => {
                 const listItem = document.createElement("div");
-                listItem.classList.add('block');
+                listItem.classList.add("block");
                 list.appendChild(listItem);
-                listItem.innerHTML = `
-            <label class="custom-checkbox">
-                <input type="checkbox" ${todo.isCompleted ? 'checked' : ''} data-id="${todo.id}">
+                listItem.innerHTML = `<label class="custom-checkbox">
+                <input type="checkbox" ${todo.isCompleted ? "checked" : ""} data-id="${todo.id}">
                 <span class="checkmark"></span>
                 ${todo.name}
             </label>
-        `;
-                const checkbox = listItem.querySelector('input[type="checkbox"]');
-                checkbox === null || checkbox === void 0 ? void 0 : checkbox.addEventListener('change', (event) => __awaiter(this, void 0, void 0, function* () {
-                    var _a, _b;
-                    const target = event.target;
-                    const isChecked = target.checked;
-                    const todoId = target.getAttribute('data-id');
+            <img src="addButton.PNG" class="delete-icon" data-id="${todo.id}">`;
+                switchCheckbox(listItem);
+                const deleteBtn = listItem.querySelector(".delete-icon");
+                deleteBtn === null || deleteBtn === void 0 ? void 0 : deleteBtn.addEventListener("click", () => __awaiter(this, void 0, void 0, function* () {
+                    const todoId = deleteBtn.getAttribute("data-id");
                     try {
                         yield fetch(`/todoList/${todoId}`, {
-                            method: 'PUT',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                id: todoId,
-                                isCompleted: isChecked,
-                                name: ((_b = (_a = target.closest('label')) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.trim()) || ''
-                            })
+                            method: "DELETE",
                         });
-                        console.log(`Task ${todoId} done!`);
+                        listItem.remove(); // Видаляємо елемент із DOM
+                        console.log(`Задача ${todoId} видалена`);
                     }
                     catch (error) {
-                        console.error('Task error:', error);
+                        console.error("Помилка при видаленні задачі:", error);
                     }
                 }));
             });
@@ -56,38 +46,96 @@ function fetchTodos() {
 }
 function createTodo() {
     return __awaiter(this, void 0, void 0, function* () {
-        const todoNameInput = document.getElementById('todoName');
-        const todoList = document.getElementById('todoList');
+        const todoNameInput = document.getElementById("todoName");
+        const todoList = document.getElementById("todoList");
         const todoName = todoNameInput.value.trim();
-        if (todoName === '') {
-            alert('Please, fill all fields!');
+        if (todoName === "") {
+            alert("Please, fill all fields!");
             return;
         }
         const newTodo = {
             name: todoName,
-            isCompleted: false
+            isCompleted: false,
         };
         try {
-            const response = yield fetch('/todoList', {
-                method: 'POST',
+            const response = yield fetch("/todoList", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(newTodo)
+                body: JSON.stringify(newTodo),
             });
             if (!response.ok) {
-                throw new Error('Failed to add a new task');
+                throw new Error("Failed to add a new task");
             }
-            fetchTodos;
+            yield fetchTodos;
         }
         catch (error) {
             console.error("Error:", error);
         }
-        // Очищення інпута після додавання
+        todoNameInput.value = "";
+    });
+}
+function switchCheckbox(listItem) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const checkbox = listItem.querySelector('input[type="checkbox"]');
+        checkbox === null || checkbox === void 0 ? void 0 : checkbox.addEventListener("change", (event) => __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            const target = event.target;
+            const isChecked = target.checked;
+            const todoId = target.getAttribute("data-id");
+            try {
+                yield fetch(`/todoList/${todoId}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        id: todoId,
+                        isCompleted: isChecked,
+                        name: ((_b = (_a = target.closest("label")) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.trim()) || "",
+                    }),
+                });
+                console.log(`Task ${todoId} done!`);
+            }
+            catch (error) {
+                console.error("Task error:", error);
+            }
+        }));
+    });
+}
+function deleteTodo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const todoNameInput = document.getElementById("todoName");
+        const todoList = document.getElementById("todoList");
+        const todoName = todoNameInput.value.trim();
+        if (todoName === "") {
+            alert("Please, fill all fields!");
+            return;
+        }
+        const newTodo = {
+            name: todoName,
+            isCompleted: false,
+        };
+        try {
+            const response = yield fetch("/todoList", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newTodo),
+            });
+            if (!response.ok) {
+                throw new Error("Failed to add a new task");
+            }
+        }
+        catch (error) {
+            console.error("Error:", error);
+        }
         todoNameInput.value = "";
     });
 }
 window.onload = fetchTodos;
-(_a = document.getElementById('addTodoBtn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', createTodo);
+(_a = document.getElementById("addTodoBtn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", createTodo);
 export {};
 //# sourceMappingURL=app.js.map
--- Todo.TwilightSaw/wwwroot/ts/app.ts
+++ Todo.TwilightSaw/wwwroot/ts/app.ts
@@ -1,45 +1,37 @@
-﻿import { Todo } from "./Todo.js";
+import { Todo } from "./Todo.js";
 
 async function fetchTodos(): Promise<void> {
-    const response = await fetch('/todoLists');
+    const response = await fetch("/todoLists");
     const todo: Todo[] = await response.json();
 
-    const list = document.getElementById('todoList')!;
+    const list = document.getElementById("todoList")!;
     if (list) {
-        list.innerHTML = '';
+        list.innerHTML = "";
         todo.forEach((todo: Todo) => {
             const listItem = document.createElement("div");
-            listItem.classList.add('block');
+            listItem.classList.add("block");
 
             list.appendChild(listItem);
-            listItem.innerHTML = `
-            <label class="custom-checkbox">
-                <input type="checkbox" ${todo.isCompleted ? 'checked' : ''} data-id="${todo.id}">
+            listItem.innerHTML = `<label class="custom-checkbox">
+                <input type="checkbox" ${todo.isCompleted ? "checked" : ""} data-id="${todo.id}">
                 <span class="checkmark"></span>
                 ${todo.name}
             </label>
-        `;
+            <img src="addButton.PNG" class="delete-icon" data-id="${todo.id}">`;
+            switchCheckbox(listItem);
 
-            const checkbox = listItem.querySelector('input[type="checkbox"]');
-            checkbox?.addEventListener('change', async (event) => {
-                const target = event.target as HTMLInputElement;
-                const isChecked = target.checked;
-                const todoId = target.getAttribute('data-id');
-              
+            const deleteBtn = listItem.querySelector(".delete-icon");
+            deleteBtn?.addEventListener("click", async () => {
+                const todoId = deleteBtn.getAttribute("data-id");
+
                 try {
                     await fetch(`/todoList/${todoId}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            id: todoId,
-                            isCompleted: isChecked,
-                            name: target.closest('label')?.textContent?.trim() || '' })
+                        method: "DELETE",
                     });
-                    console.log(`Task ${todoId} done!`);
+                    listItem.remove(); // Видаляємо елемент із DOM
+                    console.log(`Задача ${todoId} видалена`);
                 } catch (error) {
-                    console.error('Task error:', error);
+                    console.error("Помилка при видаленні задачі:", error);
                 }
             });
         });
@@ -47,44 +39,106 @@ async function fetchTodos(): Promise<void> {
 }
 
 async function createTodo(): Promise<void> {
-    const todoNameInput = document.getElementById('todoName') as HTMLInputElement;
-    const todoList = document.getElementById('todoList');
+    const todoNameInput = document.getElementById(
+        "todoName"
+    ) as HTMLInputElement;
+    const todoList = document.getElementById("todoList");
 
     const todoName = todoNameInput.value.trim();
 
-    if (todoName === '') {
-        alert('Please, fill all fields!');
+    if (todoName === "") {
+        alert("Please, fill all fields!");
         return;
     }
 
     const newTodo: Todo = {
         name: todoName,
-        isCompleted: false
+        isCompleted: false,
     };
 
     try {
-        const response = await fetch('/todoList', {
-            method: 'POST',
+        const response = await fetch("/todoList", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(newTodo)
+            body: JSON.stringify(newTodo),
         });
 
         if (!response.ok) {
-            throw new Error('Failed to add a new task');
+            throw new Error("Failed to add a new task");
         }
 
-        fetchTodos;
-
+        await fetchTodos;
     } catch (error) {
         console.error("Error:", error);
     }
+    todoNameInput.value = "";
+}
 
-    // Очищення інпута після додавання
+async function switchCheckbox(listItem: HTMLElement): Promise<void> {
+    const checkbox = listItem.querySelector('input[type="checkbox"]');
+    checkbox?.addEventListener("change", async (event) => {
+        const target = event.target as HTMLInputElement;
+        const isChecked = target.checked;
+        const todoId = target.getAttribute("data-id");
+
+        try {
+            await fetch(`/todoList/${todoId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: todoId,
+                    isCompleted: isChecked,
+                    name: target.closest("label")?.textContent?.trim() || "",
+                }),
+            });
+            console.log(`Task ${todoId} done!`);
+        } catch (error) {
+            console.error("Task error:", error);
+        }
+    });
+}
+
+async function deleteTodo(): Promise<void> {
+    const todoNameInput = document.getElementById(
+        "todoName"
+    ) as HTMLInputElement;
+    const todoList = document.getElementById("todoList");
+
+    const todoName = todoNameInput.value.trim();
+
+    if (todoName === "") {
+        alert("Please, fill all fields!");
+        return;
+    }
+
+    const newTodo: Todo = {
+        name: todoName,
+        isCompleted: false,
+    };
+
+    try {
+        const response = await fetch("/todoList", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTodo),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to add a new task");
+        }
+
+        
+    } catch (error) {
+        console.error("Error:", error);
+    }
     todoNameInput.value = "";
 }
 
 window.onload = fetchTodos;
-document.getElementById('addTodoBtn')?.addEventListener('click', createTodo);
-
+document.getElementById("addTodoBtn")?.addEventListener("click", createTodo);
