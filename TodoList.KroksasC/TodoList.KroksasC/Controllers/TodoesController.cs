81e0eaf37911c9b2f08ed86d1ce0d9d7a6ada163
Gustas Čemrka <gustascemerka@gmail.com>
2025-01-30 16:04:06 +0200

    Finished

---
+++ TodoList.KroksasC/TodoList.KroksasC/Controllers/TodoesController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoList.KroksasC.Data;
+using TodoList.KroksasC.Model;
+
+namespace TodoList.KroksasC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoesController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoesController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodo()
+        {
+            return await _context.Todo.ToListAsync();
+        }
+
+        // GET: api/Todoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodo(int id)
+        {
+            var todo = await _context.Todo.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        // PUT: api/Todoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodo(int id, Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+        {
+            _context.Todo.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+        }
+
+        // DELETE: api/Todoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(int id)
+        {
+            var todo = await _context.Todo.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todo.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todo.Any(e => e.Id == id);
+        }
+    }
+}
